f23aff823341755b5cd37b2ac25786e173cd455b
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-04-26 11:39:40 +0200

    Added header

--- public/themes/samjobb/templates/support-matching.php
+++ public/themes/samjobb/templates/support-matching.php
@@ -1,9 +1,20 @@
 <?php
 /*
-Template Name: Support och matchning
+Template Name: Stöd & Matchning
 */
 ?>
 
 <?php get_header(); ?>
+
+</div> <!-- End of container div -->
+
+<!-- Header -->
+<div class="header">
+    <img class="home-header" src="<?php the_field('header'); ?>" />
+    <div class="header-content">
+        <h1><?php the_field('welcome_text'); ?></h1>
+        <p><?php the_field('description_text'); ?></p>
+    </div>
+</div>
 
 <?php get_footer(); ?>
