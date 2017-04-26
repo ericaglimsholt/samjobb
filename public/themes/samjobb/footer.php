7b4dfe075d0bef7a63c6e96fed528187ee7f3c39
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-04-26 13:40:42 +0200

    Added content in footer

--- public/themes/samjobb/footer.php
+++ public/themes/samjobb/footer.php
@@ -1,6 +1,33 @@
     <!-- End of container div, starts in header.php -->
     </div>
 
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 footer">
+                <h5>Om SamJobb</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur you adipiscing you elit.Etiam vel laoreet justo, egetss eleifend lectus.Its Mausaccumsan sem isss.</p>
+                <h5>Följ SamJobb</h5>
+            </div>
+            <div class="col-md-4 footer">
+                <h5>Öppetider</h5>
+                <p>
+                    Lorem ipsum dolor sit amet, onsectetu eiusmo dostadip isicing elit, sed do eiusmod is tempinc idiuntm ut lorem ipsome do or sit amt la bore.
+                    <br><br>
+                    Monday- Friday. . . . . . . . . . . . . 8 AM - 5PM<br>
+                    Sunday. . . . . . . . . . . . . . . . . . . . . 12 AM - 5PM<br>
+                    Saturday . . . . . . . . . . . . . . . . . . Close
+                </p>
+
+            </div>
+            <div class="col-md-4 footer">
+                <h5>Kontakta oss</h5>
+            </div>
+        </div>
+    </div>
+</div>
+
+
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
