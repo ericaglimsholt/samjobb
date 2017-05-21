64073bc695dd22e6dc06a647bb683d242f2b68a4
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-05-21 14:58:20 +0200

    Tried to fix contact

--- public/themes/samjobb/templates/contact.php
+++ public/themes/samjobb/templates/contact.php
@@ -36,4 +36,30 @@ Template Name: Kontakt
     </div>
 </div>
 
+<?php
+$employees = get_posts( array (
+  'post_type' => 'employee',
+  'order' => 'ASC',
+  'orderby' => 'name',
+  'posts_per_page' => -1,
+));
+
+$the_query = new WP_Query( $employees );
+ ?>
+
+<!-- Show employee -->
+<div class="row">
+    <div class="col-md-12">
+        <h2><?php the_field('title'); ?></h2>
+    </div>
+    <?php if( $the_query->have_posts() ): ?>
+	<?php while( $the_query->have_posts() ) : $the_query->the_post(); ?>
+        <div class="col-md-4">
+            <h3><?php echo get_field ('title') ?></h3>
+        </div>
+	<?php endwhile; ?>
+<?php endif; ?>
+<?php wp_reset_query(); ?>
+</div>
+
 <?php get_footer(); ?>
