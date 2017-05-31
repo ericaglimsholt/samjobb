2efe0d8933e3a34aa957a136b9e6ab9c648293f8
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-05-31 14:31:03 +0200

    Output all the employee information

--- public/themes/samjobb/templates/contact.php
+++ public/themes/samjobb/templates/contact.php
@@ -20,7 +20,7 @@ Template Name: Kontakt
 <div class="container">
 
 <!-- Opening contact and forms -->
-<div class="row">
+<div class="row contact-row">
     <div class="col-md-6 contact">
         <h2><?php the_field('title'); ?></h2>
         <div class="forms">
@@ -37,29 +37,37 @@ Template Name: Kontakt
 </div>
 
 <?php
-$employees = get_posts( array (
-  'post_type' => 'employee',
-  'order' => 'ASC',
-  'orderby' => 'name',
-  'posts_per_page' => -1,
-));
+$employees = array (
+    'numberposts'	=> -1,
+    'post_type'		=> 'employee',
+);
 
 $the_query = new WP_Query( $employees );
  ?>
 
+ <div class="employee-hr">
+     <hr>
+ </div>
+
 <!-- Show employee -->
 <div class="row">
-    <div class="col-md-12">
-        <h2><?php the_field('title'); ?></h2>
+
+    <div class="col-md-12 employee-contact">
+        <h2><?php the_field('title_employee'); ?></h2>
     </div>
     <?php if( $the_query->have_posts() ): ?>
-	<?php while( $the_query->have_posts() ) : $the_query->the_post(); ?>
-        <div class="col-md-4">
-            <h3><?php echo get_field ('title') ?></h3>
+        <?php while( $the_query->have_posts() ) : $the_query->the_post(); ?>
+        <div class="col-md-4 employee-contact">
+            <img src="<?php the_field('image'); ?>" alt="">
+            <p class="employee-name"><?php the_field('name'); ?></p>
+            <p class="employee-work"><?php the_field('work_area'); ?></p>
+            <hr>
+            <p class="employee-description"><?php the_field('description'); ?></p>
         </div>
-	<?php endwhile; ?>
-<?php endif; ?>
+    	<?php endwhile; ?>
+    <?php endif; ?>
 <?php wp_reset_query(); ?>
 </div>
+
 
 <?php get_footer(); ?>
