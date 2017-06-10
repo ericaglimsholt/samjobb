0c2d686915b5e5a0ff49f45443c029cf494118a9
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-06-10 11:32:11 +0200

    Added template called languages

---
+++ public/themes/samjobb/templates/languages.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Template Name: Språk
+*/
+?>
+
+<?php get_header(); ?>
+
+</div> <!-- End of container div -->
+
+<!-- Header -->
+<div class="header">
+    <img class="home-header" src="<?php the_field('header'); ?>" />
+    <div class="header-content">
+        <h1><?php the_field('welcome_text'); ?></h1>
+        <p><?php the_field('description_text'); ?></p>
+    </div>
+</div>
+
+<!-- Opening info -->
+<div class="container">
+    <div class="row opening-info">
+        <div class="col-md-5 home opening-info">
+            <img class="home-opening-info" src="<?php the_field('image_opening_info'); ?>" />
+        </div>
+        <div class="col-md-7 home opening-info">
+            <h2><?php the_field('title_opening_info'); ?></h2>
+            <p><?php the_field('description_opening_info'); ?></p>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
